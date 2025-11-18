648d8ca49b625b8cc79f1bed9818c217c23c6afb
farhadRGN <farhadrgn@gmail.com>
2025-11-18 13:10:09 +0330

    Delete playback directory

--- playback/playback.py
+++
@@ -1,8 +0,0 @@
-# Getting the sentence from user
-Sentence = input("Enter Your Sentence: ")
-
-# Replacing spaces with "..."
-Sentence = Sentence.replace(" ", "...")
-
-# Printing output
-print(Sentence)
